fd6e0e7d82a9d2db3b2fe2d28d38af077d217135
raghav garg <rohanji97@gmail.com>
2018-08-30 10:26:49 +0530

    generate.sh updated

--- ass1/generate.sh
+++ ass1/generate.sh
@@ -12,33 +12,44 @@ str=""
 for i in `seq 1 $cars`;
 do
 	var="car$i.jpeg"
+	addr=$(base64 $var)
+	embedded_data="data:image/png;base64,$addr"	
 	str="$str
-	<img src=\"$var\" height=\"256\"  width=\"256 \" >
+	<img src=\"$embedded_data\" height=\"256\"  width=\"256 \" >
 	"
 done
 
 for i in `seq 1 $dogs`;
 do
 	var="dogs$i.jpeg"
+
+	addr=$(base64 $var)
+	embedded_data="data:image/png;base64,$addr"	
 	str="$str
-	<img src=\"$var\" height=\"256\"  width=\"256 \" >
+	<img src=\"$embedded_data\" height=\"256\"  width=\"256 \" >
 	"
 done
 
 for i in `seq 1 $trucks`;
 do
 	var="truck$i.jpeg"
+
+	addr=$(base64 $var)
+	embedded_data="data:image/png;base64,$addr"	
 	str="$str
-	<img src=\"$var\" height=\"256\"  width=\"256 \" >
+	<img src=\"$embedded_data\" height=\"256\"  width=\"256 \" >
 	"
 done
 
 for i in `seq 1 $cats`;
 do
 	var="cat$i.jpeg"
+
+	addr=$(base64 $var)
+	embedded_data="data:image/png;base64,$addr"	
 	str="$str
-	<img src=\"$var\" height=\"256\"  width=\"256 \" >
+	<img src=\"$embedded_data\" height=\"256\"  width=\"256 \" >
 	"
 done
 echo "$html_top$str$html_bot" >option.html
-#xdg-open option.html
+xdg-open option.html

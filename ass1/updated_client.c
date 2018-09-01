c17739b74c7bdd9ad48b66925e4fd271c47e9753
Krishna Kumar <kk4466345@gmail.com>
2018-09-01 22:24:01 +0530

    Edit server and client

--- ass1/updated_client.c
+++ ass1/updated_client.c
@@ -4,7 +4,7 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-//#include <unistd.h>
+#include <unistd.h>
 #define MAX 1700000
 #define MAX_REQ 100
 #define LENGTH 512
@@ -23,35 +23,28 @@ int main(int argc, char *argv[]){
   char revbuf[LENGTH+1], request[MAX_REQ];
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
-  //struct hostent *server;
-  //server = gethostbyname(argv[1]);
+
   int PORT = atoi(argv[2]);
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+
   clientSocket = socket(PF_INET, SOCK_STREAM, 0);
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
+ 
   serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
+
   serverAddr.sin_port = htons(PORT);
   /* Set IP address to localhost */
   serverAddr.sin_addr.s_addr = inet_addr(argv[1]);
-  //memcpy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-  /* Set all bits of the padding field to 0 */
+
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*---- Connect the socket to the server using the address struct ----*/
   addr_size = sizeof serverAddr;
   connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
 
-  /*---- Send the request to the server ----*/
+  printf("Maximum request allowed---> 4 each\n");
   printf("Enter your request eg - 3 cats 4 trucks and 1 dog :   ");
   fgets(request, MAX, stdin);
   send(clientSocket, request, sizeof(request), 0);
   printf("Request sent successfully\n");
   
-  /*---- Save server response in buffer and then paste it in .html file ----*/
-  //recv(clientSocket, buffer, MAX, 0);
   FILE *fp = fopen("response.html","w");
        bzero(revbuf, LENGTH);
         int f_block_sz = 0;
--- ass1/updated_server.c
+++ ass1/updated_server.c
@@ -18,69 +18,60 @@ int main(){
     socklen_t addr_size;
     char req[MAX];
 
-    /*---- Create the socket. The three arguments are: ----*/
-    /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+
     welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-    /*---- Configure settings of the server address struct ----*/
-    /* Address family = Internet */
     serverAddr.sin_family = AF_INET;
-    /* Set port number, using htons function to use proper byte order */
+
     serverAddr.sin_port = htons(PORT);
-    /* Set IP address to localhost */
+    
     serverAddr.sin_addr.s_addr = inet_addr("0.0.0.0");
-    /* Set all bits of the padding field to 0 */
+    
     memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-    /*---- Bind the address struct to the socket ----*/
     n = bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
     if(n<0){ 
         printf("ERROR on bind\n");
         return 0;
     }
-    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+
+
+while(1){
     if(listen(welcomeSocket,5)==0)
-        printf("I'm listening\n");
+        printf("Server_bashed initialised\n");
     else
         printf("Error\n");
 
-    /*---- Accept call creates a new socket for the incoming connection ----*/
     addr_size = sizeof serverStorage;
+    
+
+
     newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
 
-    /*---- Receive the request and accordingly generate .html file ----*/
+
+
     recv(newSocket, req, MAX, 0);
     printf("Request received successfully\n");
-    //char opening[MAX] = "<!DOCTYPE HTML>\n<html>\n<head><title>CS252</title></head>\n<body>\n<table>\n", temp[2]="0";
-    //char *closing = "</table>\n</body>\n</html>";
+   
     char *token = strtok(req, " "), image_name[5];
     int quantity=0;
     int cats=0, cars=0, dogs=0, trucks=0;
     while(token != NULL){
         if(strlen(token)==1 && token[0]>='1' && token[0]<='4') quantity=token[0]-'0';
         else if(strcmp(token,"and")!=0){
-            if(strncmp(token,"dog",3)==0) //strcpy(image_name,"dog");
+            if(strncmp(token,"dog",3)==0) 
                 dogs += quantity;
-            if(strncmp(token,"cat",3)==0) //strcpy(image_name,"cat");
+            if(strncmp(token,"cat",3)==0) 
                 cats+=quantity;
-            if(strncmp(token,"car",3)==0) //strcpy(image_name,"car");
+            if(strncmp(token,"car",3)==0) 
                 cars+=quantity;
-            if(strncmp(token,"tru",3)==0) //strcpy(image_name,"truck");
+            if(strncmp(token,"tru",3)==0)
                 trucks+=quantity;
-            /*for(int i=1;i<=quantity;i++){
-                strcat(opening,"<tr>\n<td>");
-                temp[0]=i+'0';
-                strcat(opening,temp);
-                strcat(opening,"</td>\n<td><img src=\"images/");
-                strcat(opening,image_name);
-                strcat(opening,temp);
-                strcat(opening,".jpeg");
-                strcat(opening,"\" height=\"60\" width=\"60\"></td>\n</tr>\n");
-            }*/
+      
         }        
         token = strtok(NULL, " ");
     }
-    //strcat(opening,closing);
+  
     char *buffer=(char*)malloc(sizeof(int));
     char str[]="bash generate.sh ";
     char *sdbuf=NULL;
@@ -94,8 +85,7 @@ int main(){
     strcat(str,buffer);
     system(str);
     printf("file generated successfully\n");
-    /*FILE *fp = fopen("option.html", "ab+");
-    fread(fileData, 1, MAX, fp);*/
+    
     FILE *fp = fopen("option.html","r");
     fseek(fp, 0, SEEK_END);
     long fsize = ftell(fp);
@@ -107,21 +97,20 @@ int main(){
             {
                 if(send(newSocket, sdbuf, f_block_sz, 0) < 0)
                 {
-                   // printf("ERROR: Failed to send file %s.\n", f_name);
+                    printf("File sent failed");
                     break;
                 }
                 bzero(sdbuf, LENGTH);
             }
-   /* FILE *fptr = fopen("option.html","r");
-    fseek(fptr, 0, SEEK_END);
-    long fsize = ftell(fptr);
-    fseek(fptr, 0, SEEK_SET);
-    fileData = malloc(fsize + 1);
-    fread(fileData, fsize, 1, fptr);
-    fclose(fptr);	
-    fileData[fsize] = 0;
-    send(newSocket, fileData, fsize, 0); */ 
+
     printf("Data sent to client\n");
+   close(newSocket);
+    system("clear");
+
+   
+
+}
+
     close(welcomeSocket);
     return 0;
 }

30b28af824d42aeb16166e6e0e269617f7071df5
Shivanshu Singh <shivnshus@gmail.com>
2018-11-02 13:35:40 +0530

    Complete reset password functionality

--- assignment4/includes/ret.inc.php
+++ assignment4/includes/ret.inc.php
@@ -21,18 +21,20 @@ include_once 'db_connect.php';
 include_once 'psl-config.php';
 
 $error_msg = "";
-$suggestions=array();
 
-
-
-if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['username'], $_POST['email'], $_POST['p'])) {
     // Sanitize and validate the data passed in
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
-    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-    $email = filter_var($email, FILTER_VALIDATE_EMAIL);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        // Not a valid email
-        $error_msg .= '<p class="error">The email address you entered is not valid</p>';
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $secret = $_GET['secret'];
+
+    $sq = 'SELECT salt, email FROM members WHERE username="'.$username.'";';
+    $result = $mysqli->query($sq);
+    if ($row = $result->fetch_assoc()) {
+        if ($row["salt"] != $secret) {
+            echo "<script>alert('Invalid secret value.');</script>";
+            exit();
+        }
     }
 
     $password = filter_input(INPUT_POST, 'p', FILTER_SANITIZE_STRING);
@@ -42,87 +44,27 @@ if (isset($_POST['username'], $_POST['email'], $_POST['p'])) {
         $error_msg .= '<p class="error">Invalid password configuration.</p>';
     }
 
-    // Username validity and password validity have been checked client side.
-    // This should should be adequate as nobody gains any advantage from
-    // breaking these rules.
-    //
 
-    $prep_stmt = "SELECT id FROM members WHERE email = ? LIMIT 1";
-    $stmt = $mysqli->prepare($prep_stmt);
+    // Create a random salt
+    $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
 
-    if ($stmt) {
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $stmt->store_result();
-
-        if ($stmt->num_rows == 1) {
-            // A user with this email address already exists
-            $error_msg .= '<p class="error">A user with this email address already exists.</p>';
-        }
-    } else {
-        $error_msg .= '<p class="error">Database error</p>';
-    }
-
-    $prep_stmt = "SELECT id FROM members WHERE username = ? LIMIT 1";
-    $stmt = $mysqli->prepare($prep_stmt);
-
-    if ($stmt) {
-        $stmt->bind_param('s', $username);
-        $stmt->execute();
-        $stmt->store_result();
-
-        if ($stmt->num_rows == 1) {
-            // A user with this email address already exists
-            $error_msg .= '<p class="error">A user with this username address already exists.</p>';
-            $sq = "SELECT * FROM members WHERE username REGEXP '^".$username."[0-9]+'";
-            $result = $mysqli->query($sq);
-            //$res->execute();
-            //$res->store_result();
-            $names_left=5;
-            $primes=array(1087,1103,1181,1277,1289,1409,1511,1583,1619,1697,1721,1787,1823,1879,1907,1949,2027);
-            $taken=array();
-            $x=0;
-
-            while($row = $result->fetch_assoc()) {
-                 $taken[$x]=$row["username"];
-                 $x=$x+1;
-            }
-            while($names_left>0){
-                $random_keys=array_rand($primes,1);
-                $count=(rand(1,100)*10000)%$primes[$random_keys];
-                if( !in_array($username . $count, $taken) ){
-                    $suggestions[] = $username . $count;
-                    $names_left--;
-                }
-            }
-
-
-        }
-    } else {
-        $error_msg .= '<p class="error">Database error</p>';
-    }
-    // TODO:
-    // We'll also have to account for the situation where the user doesn't have
-    // rights to do registration, by checking what type of user is attempting to
-    // perform the operation.
-
-    if (empty($error_msg)) {
-        // Create a random salt
-        $random_salt = hash('sha512', uniqid(openssl_random_pseudo_bytes(16), TRUE));
-
-        // Create salted password
+    // Create salted password
         $password = hash('sha512', $password . $random_salt);
 
-        // Insert the new user into the database
-        if ($insert_stmt = $mysqli->prepare("INSERT INTO members (username, email, password, salt) VALUES (?, ?, ?, ?)")) {
-            $insert_stmt->bind_param('ssss', $username, $email, $password, $random_salt);
-            // Execute the prepared query.
-            if (! $insert_stmt->execute()) {
-                header('Location: ../error.php?err=Registration failure: INSERT');
-                exit();
-            }
+        $email = '"'.$email.'"';
+        $password = '"'.$password.'"';
+        $random_salt = '"'.$random_salt.'"';
+
+        $qr = "UPDATE members SET password = ".$password.", salt = ".$random_salt." WHERE email = ".$email."";
+        $update_stmt = $mysqli->prepare($qr);
+
+        if ($update_stmt->execute()) {
+            echo "<script>alert('Password reset successful.');</script>";
+        } else {
+            echo "<script>alert('Password reset failed.');</script>";
         }
-        header('Location: ./register_success.php');
+
+        echo "<script type='text/javascript'>document.location.href='../index.php';</script>";
+        echo '<META HTTP-EQUIV="refresh" content="0;URL=../index.php">';
         exit();
-    }
 }
